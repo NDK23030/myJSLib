a5d6af2732335054a86494f20a0dd6ae3669fc61
Denis Mazanov <ggmazanovdenis@gmail.com>
2023-06-15 15:39:18 +0300

    create dropdown menu component

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,17 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).onClick(() => {
     $('.w-500').fadeToggle(800);
 });
+
+$('.wrap').html(
+    `<div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+            <a href="#" class="dropdown-item">action</a>
+            <a href="#" class="dropdown-item">action #2</a>
+            <a href="#" class="dropdown-item">action #3</a>
+        </div>
+
+    </div>`
+);
+
+$('.dropdown-toggle').dropdown();
